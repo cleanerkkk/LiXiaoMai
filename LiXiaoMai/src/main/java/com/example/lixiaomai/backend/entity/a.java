e682fcf1cdfa9c671207bcf6b6939833d22ac6a8
cleanerkkk <1963795880@qq.com>
2024-07-17 21:50:42 +0800

    1

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/entity/a.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/entity/a.java
@@ -1,5 +1,7 @@
 package main.java.com.example.lixiaomai.backend.entity;
 
 public class a {
-    a
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
