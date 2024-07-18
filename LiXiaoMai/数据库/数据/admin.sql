ff08af703fc92a00b04ba262841459dc5c31f281
lyx0902 <320979308@qq.com>
2024-07-18 20:10:52 +0800

    刘宇翔

---
+++ LiXiaoMai/数据库/数据/admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO admin (`id`, `password`, `uname`) VALUES (1, '1111', 'a');
+INSERT INTO admin (`id`, `password`, `uname`) VALUES (2, '2222', 'b');
+INSERT INTO admin (`id`, `password`, `uname`) VALUES (3, '3333', 'c');
+INSERT INTO admin (`id`, `password`, `uname`) VALUES (4, '4444', 'd');
+INSERT INTO admin (`id`, `password`, `uname`) VALUES (5, '5555', 'e');
