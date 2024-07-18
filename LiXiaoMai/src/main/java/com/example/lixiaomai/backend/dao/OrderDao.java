2d4b086f48afcc3bf8d3be872fa8456859483cc9
cleanerkkk <1963795880@qq.com>
2024-07-18 21:03:21 +0800

    1

---
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/dao/OrderDao.java
@@ -0,0 +1,9 @@
+package com.example.lixiaomai.backend.dao;
+
+import com.example.lixiaomai.backend.tools.DatabaseUtils;
+import org.apache.commons.dbutils.QueryRunner;
+
+public class OrderDao {
+    private final QueryRunner runner = DatabaseUtils.getRunner();
+
+}
