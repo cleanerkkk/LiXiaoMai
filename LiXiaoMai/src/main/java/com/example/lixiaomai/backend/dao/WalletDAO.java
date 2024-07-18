6fed4bd89cc224bbbfeefa38b46320d29dca2f34
lyx0902 <320979308@qq.com>
2024-07-18 21:03:01 +0800

    刘宇翔

---
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/dao/WalletDAO.java
@@ -0,0 +1,11 @@
+package com.example.lixiaomai.backend.dao;
+import com.example.lixiaomai.backend.entity.Wallet;
+import com.example.lixiaomai.backend.tools.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+public class WalletDAO {
+
+
+}
