cefd4eeb2b011349548b70841f32f83cd2dfe4bc
cleanerkkk <1963795880@qq.com>
2024-07-18 22:04:26 +0800

    1

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/dao/OrderDao.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/dao/OrderDao.java
@@ -2,6 +2,7 @@ package com.example.lixiaomai.backend.dao;
 
 import com.example.lixiaomai.backend.entity.Order;
 import com.example.lixiaomai.backend.tools.DatabaseUtils;
+import com.example.lixiaomai.backend.tools.Tool;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
@@ -22,14 +23,70 @@ public class OrderDao {
             throw new RuntimeException(e);
         }
     }
+    public List<Order> getAllOrderByDeliverId(int did){
+        try{
+            Connection conn = DatabaseUtils.getConnection();
+            String sql = "SELECT * FROM ORDER WHERE did = ?";
+            return runner.query(conn, sql, new BeanListHandler<>(Order.class), did);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public List<Order> getAllOrderBySid(int sId){
+        try{
+            Connection conn = DatabaseUtils.getConnection();
+            String sql = "SELECT * FROM ORDER WHERE sid = ?";
+            return runner.query(conn, sql, new BeanListHandler<>(Order.class), sId);
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
 
     public boolean addOrder(Order order){
         try{
             Connection conn = DatabaseUtils.getConnection();
-            String sql = "INSERT INTO ORDER (ID, CID, SID, GID, GOODSNUM, ENDTIME, STARTTIME, TOTAL, STATUS, SNAME, DISCOUNTNUM, DID, CNAME)" +
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            return runner.update(conn, sql, order.getId(), order.getCId(), order.getSId(), order.getGId(), order.getGoodsNum(), order.getEndTime(),
-                    order.getStartTime(), order.getTotal(), order.getStatus(), order.getSName(), order.getDiscountNum(), order.getDiscountId(), order.getCName()) > 0;
+            String sql = "INSERT INTO ORDER (CID, SID, GID, GOODSNUM, ENDTIME, STARTTIME, TOTAL, STATUS, SNAME, DISCOUNTNUM, DID, CNAME) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            String gId = Tool.ListToString(order.getGId());
+            String goodsNum = Tool.ListToString(order.getGoodsNum());
+            String discountId = Tool.ListToString(order.getDiscountId());
+            String discountNum = Tool.ListToString(order.getDiscountNum());
+            return runner.update(conn, sql,
+                    order.getCId(), order.getSId(), gId, goodsNum, order.getEndTime(), order.getStartTime(), order.getTotal(), order.getStatus(), order.getSName(), discountNum, discountId, order.getCName()) > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean updateOrder(Order order){
+        try{
+            Connection conn = DatabaseUtils.getConnection();
+            String sql = "UPDATE ORDER SET CID = ?, SID = ?, GID = ?, GOODSNUM = ?, ENDTIME = ?, STARTTIME = ?, TOTAL = ?, STATUS = ?, SNAME = ?, DISCOUNTNUM = ?, DID = ?, CNAME = ? WHERE ID = ?";
+            String gId = Tool.ListToString(order.getGId());
+            String goodsNum = Tool.ListToString(order.getGoodsNum());
+            String discountId = Tool.ListToString(order.getDiscountId());
+            String discountNum = Tool.ListToString(order.getDiscountNum());
+            return runner.update(conn, sql,
+                    order.getCId(), order.getSId(), gId, goodsNum, order.getEndTime(), order.getStartTime(), order.getTotal(), order.getStatus(), order.getSName(), discountNum, discountId, order.getCName(), order.getId()) > 0;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean delOrderById(int id){
+        try{
+            Connection conn = DatabaseUtils.getConnection();
+            String sql = "DELETE FROM ORDER WHERE ID = ?";
+            return runner.update(conn, sql, id) > 0;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Order getOrderById(int id){
+        try{
+            Connection conn = DatabaseUtils.getConnection();
+            String sql = "SELECT * FROM ORDER WHERE ID = ?";
+            return runner.query(conn, sql, new BeanHandler<>(Order.class), id);
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
