e80859e6827b6978b5903771cf50af54ceb312c8
Alan10086 <x2584087792@163.com>
2024-07-18 20:23:46 +0800

    sql

---
+++ LiXiaoMai/数据库/数据/product.sql
@@ -0,0 +1,14 @@
+INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID)
+VALUES (1, '麦辣鸡腿堡', 22, 101, '主食', '金黄脆辣的外皮，鲜嫩幼滑的鸡腿肉', 1);
+
+INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID)
+VALUES (2, '奥利奥麦旋风', 14, 11, '甜品', '奇妙的黑白搭配，为孩子提供更多营养', 1);
+
+INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID)
+VALUES (3, '脆脆薯条', 15, 201, '小食', '精选优质土豆，美味无人能挡', 1);
+
+INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID)
+VALUES (4, '香辣鸡翅', 12, 21, '小食', '鲜美多汁，一口热辣过瘾', 2);
+
+INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID)
+VALUES (3, '抹茶软雪星冰乐', 38, 7, '饮品', '精选宁夏牛乳，与香草籽搅打成沁甜雪乳质地', 3);
