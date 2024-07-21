cf7f244004fcf3e864915780bcc0d6165afd7dd6
lyx0902 <320979308@qq.com>
2024-07-21 17:14:23 +0800

    刘宇翔

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/ProfileSettingsServlet.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/ProfileSettingsServlet.java
@@ -6,12 +6,13 @@ import com.example.lixiaomai.backend.service.CustomerService;
 import com.example.lixiaomai.backend.service.DelivermanService;
 import com.example.lixiaomai.backend.service.OrderService;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
-
+@WebServlet("/ProfileSettingsServlet")
 public class ProfileSettingsServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
