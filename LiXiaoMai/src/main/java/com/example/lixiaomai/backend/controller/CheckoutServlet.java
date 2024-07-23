5bd40c75ee1ae31879b798bd26f595416de0634b
cleanerkkk <1963795880@qq.com>
2024-07-23 20:26:15 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	LiXiaoMai/数据库/create.sql

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/CheckoutServlet.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/CheckoutServlet.java
@@ -2,6 +2,7 @@ package com.example.lixiaomai.backend.controller;
 
 import com.example.lixiaomai.backend.entity.Cart;
 import com.example.lixiaomai.backend.service.CartService;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet("/checkout")
 public class CheckoutServlet extends HttpServlet {
@@ -20,6 +23,16 @@ public class CheckoutServlet extends HttpServlet {
         HttpSession session = request.getSession();
         Integer id = (Integer) session.getAttribute("id");
         CartService cartService = new CartService();
+
+        Map<Integer, List<Pair<Integer,Integer>>> map = cartService.diffProducts(cartService.getCartByCid(id));
+
+        for (Map.Entry<Integer, List<Pair<Integer,Integer>>> entry : map.entrySet()) {
+            int sId = entry.getKey();
+            List<Pair<Integer,Integer>> list = entry.getValue();
+            Order
+        }
+
+
 
 
     }
