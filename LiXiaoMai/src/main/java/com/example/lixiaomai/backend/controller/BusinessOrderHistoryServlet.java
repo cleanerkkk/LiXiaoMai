e439ddb1320ea3e4790c7bccf7a367dfbfd531d5
lyx0902 <320979308@qq.com>
2024-07-24 00:51:54 +0800

    刘宇翔

---
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/BusinessOrderHistoryServlet.java
@@ -0,0 +1,8 @@
+package com.example.lixiaomai.backend.controller;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/businessOrderHistory")
+public class BusinessOrderHistoryServlet {
+
+}
