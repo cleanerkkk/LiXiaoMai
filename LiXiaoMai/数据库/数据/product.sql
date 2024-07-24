4c247d473bebc665e9ad5593f54843359b8b7a8c
lyx0902 <320979308@qq.com>
2024-07-24 20:48:07 +0800

    刘宇翔

--- LiXiaoMai/数据库/数据/product.sql
+++ LiXiaoMai/数据库/数据/product.sql
@@ -1,15 +1,15 @@
 
 INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID,URL)
-VALUES (1, '麦辣鸡腿堡', 22, 101, '主食', '金黄脆辣的外皮，鲜嫩幼滑的鸡腿肉', 1,"LiXiaoMai/src/main/webapp/imgsrc/product1.jpg");
+VALUES (1, '麦辣鸡腿堡', 22, 101, '主食', '金黄脆辣的外皮，鲜嫩幼滑的鸡腿肉', 1,"./LiXiaoMai/src/main/webapp/imgsrc/product1.jpg");
 
 INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID,URL)
-VALUES (2, '奥利奥麦旋风', 14, 11, '甜品', '奇妙的黑白搭配，为孩子提供更多营养', 1,"LiXiaoMai/src/main/webapp/imgsrc/product2.jpg");
+VALUES (2, '奥利奥麦旋风', 14, 11, '甜品', '奇妙的黑白搭配，为孩子提供更多营养', 1,"./LiXiaoMai/src/main/webapp/imgsrc/product2.jpg");
 
 INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID,URL)
-VALUES (3, '脆脆薯条', 15, 201, '小食', '精选优质土豆，美味无人能挡', 1,"LiXiaoMai/src/main/webapp/imgsrc/product3.jpg");
+VALUES (3, '脆脆薯条', 15, 201, '小食', '精选优质土豆，美味无人能挡', 1,"./LiXiaoMai/src/main/webapp/imgsrc/product3.jpg");
 
 INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID,URL)
-VALUES (4, '香辣鸡翅', 12, 21, '小食', '鲜美多汁，一口热辣过瘾', 2,"LiXiaoMai/src/main/webapp/imgsrc/product4.jpg");
+VALUES (4, '香辣鸡翅', 12, 21, '小食', '鲜美多汁，一口热辣过瘾', 2,"./LiXiaoMai/src/main/webapp/imgsrc/product4.jpg");
 
 INSERT INTO PRODUCT (ID, NAME, PRICE, STOCK, TYPE, DESCRIPTION, SID,URL)
-VALUES (5, '抹茶软雪星冰乐', 38, 7, '饮品', '精选宁夏牛乳，与香草籽搅打成沁甜雪乳质地', 3,"LiXiaoMai/src/main/webapp/imgsrc/product5.jpg");
+VALUES (5, '抹茶软雪星冰乐', 38, 7, '饮品', '精选宁夏牛乳，与香草籽搅打成沁甜雪乳质地', 3,"./LiXiaoMai/src/main/webapp/imgsrc/product5.jpg");
