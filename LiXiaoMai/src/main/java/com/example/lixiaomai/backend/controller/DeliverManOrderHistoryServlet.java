5670d68a667ae29f04b977e1568675be07957bc2
lyx0902 <320979308@qq.com>
2024-07-24 21:26:07 +0800

    刘宇翔

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/DeliverManOrderHistoryServlet.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/DeliverManOrderHistoryServlet.java
@@ -1,4 +1,23 @@
 package com.example.lixiaomai.backend.controller;
 
-public class DeliverManOrderHistoryServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/deliverManOrder")
+public class DeliverManOrderHistoryServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        HttpSession session = request.getSession();
+        String userType = (String) session.getAttribute("userType");
+        String userName = (String) session.getAttribute("name");
+
+
+    }
 }
