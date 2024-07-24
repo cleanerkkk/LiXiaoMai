e610c644102caef5ed073b8280d3a7c476682f0f
lyx0902 <320979308@qq.com>
2024-07-24 21:27:38 +0800

    刘宇翔

--- LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/DeliverOrderHistoryServlet.java
+++ LiXiaoMai/src/main/java/com/example/lixiaomai/backend/controller/DeliverOrderHistoryServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/deliverManOrder")
-public class DeliverManOrderHistoryServlet extends HttpServlet {
+public class DeliverOrderHistoryServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
